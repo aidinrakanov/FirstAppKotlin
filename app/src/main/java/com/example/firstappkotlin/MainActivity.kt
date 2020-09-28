c08bac87ec8a1d5e265422fa6c58953eebba9073
aidinrakanov <altynaikanatbekova@gmail.com>
2020-09-28 19:02:22 +0600

    first

--- app/src/main/java/com/example/firstappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstappkotlin/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.firstappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
+
+    var name: String = "Please enter text"
+    var REQUEST_KEY: Int = 77
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
+
+        private fun toast(name: String) {
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+        }
+
+        fun btnFirstActivity(view: View) {
+            val textFirstActivity = first_text.text.toString().trim()
+            if (textFirstActivity.isNotEmpty()) {
+                startActivityForResult(
+                    Intent(this, Second::class.java).putExtra("key", textFirstActivity)
+                    , REQUEST_KEY
+                )
+            } else {
+                toast(name)
+            }
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == REQUEST_KEY && resultCode == RESULT_OK && data != null) {
+                val newText = data.getStringExtra("key")
+                text_from2.text = newText
+            }
+        }
+    }}
--- app/src/main/java/com/example/firstappkotlin/Second.kt
+++ app/src/main/java/com/example/firstappkotlin/Second.kt
@@ -2,10 +2,29 @@ package com.example.firstappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_second.*
 
 class Second : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
-    }
+        lateinit var text: String
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_second)
+            showTextFirstActivity()
+        }
+
+        private fun showTextFirstActivity() {
+            val getText = intent
+            text = getText.getStringExtra("key") as String
+            second_text.setText(text)
+        }
+
+        fun onSendBack(view: View) {
+            val newText = second_text.text.toString().trim()
+            if (newText.isNotEmpty()) {
+                intent.putExtra("key", newText)
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+        }
 }
